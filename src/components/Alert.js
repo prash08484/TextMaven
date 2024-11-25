27415ad3301c41fc7461b445a29c628d81beb826
Prashant Yadav <prashanty.ug23.cs@nitp.ac.in>
2024-11-25 17:15:46 +0530

    adding whole project

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,14 +1,10 @@
 import React from 'react'
-
 function Alert(props) {
-  return (
-    // <div> 
-props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
-  <strong> {props.alert.type.toUpperCase()}</strong>   {props.alert.msg}
-  <button type="button" className="btn-close" data-bs-dismiss ="alert" aria-label="Close"></button>
-</div>
-    // </div>
+  return ( 
+    props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+      <strong> {props.alert.type.toUpperCase()}</strong>   {props.alert.msg}
+      <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div> 
   )
 }
-
 export default Alert
